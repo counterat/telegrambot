331c2765032377a461d9121fa2cc5ea53ab12a0c
Yuriy Zholtov <125594698+counterat@users.noreply.github.com>
2024-02-07 00:59:21 +0200

    Update buttons.py

--- buttons.py
+++ buttons.py
@@ -18,6 +18,7 @@ async def main_buttons(user: User):
             Butt_main.add(types.KeyboardButton(e.emojize(f":green_circle: До: {dateto} МСК:green_circle:")))
 
         Butt_main.add(types.KeyboardButton(e.emojize(f"Продлить :money_bag:")),types.KeyboardButton(e.emojize(f"Подключить VPN :gear:")))
+        Butt_main.add(types.KeyboardButton(e.emojize("Пригласить друга:handshake:")))
         Butt_main.add(types.KeyboardButton(e.emojize(f"FAQ :thinking_face:")))
 
         if CONFIG["admin_tg_id"] == user.tgid:
